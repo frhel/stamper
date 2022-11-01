bd39b27f00f398f7932d1348f0755ce57d430af0
Freyr H <freyr.helga@gmail.com>
2022-11-01 18:55:20 +0000

    - Fetch song queue from server - Process current song into currentSong object - Read played history from server and save to songHistory object - Initiate websocket connection to streamersonglist - Create song-handler.js to deal with songlist specific processing - Set up websocket listeners to update currentSong and songHistory obj

---
+++ app.js
@@ -0,0 +1,40 @@
+import io from 'socket.io-client';
+import { updateCurrentSong, getSongHistory } from './song-handler.js';
+
+// https://api.streamersonglist.com/docs/ for endpoints. 
+const streamerId = 7325;
+
+let currentSong = {};
+let songHistory = {};
+
+async function setInitialValues() {
+    currentSong = await updateCurrentSong();
+    songHistory = await getSongHistory();
+    pushButton();
+}
+
+const client = io(`https://api.streamersonglist.com`, {
+    transports: ["websocket"]
+});
+
+client.on('connect', () => {
+    console.log(`Socket.io-client connection initialized with client Id: ${client.id}`);
+    // streamerId is the numeric `id` from `/streamers/<streamer-name` endpoint
+    // but needs to be cast as a string for the socket event
+    client.emit('join-room', `${streamerId}`);
+    
+});
+client.on('queue-update', async () => {
+    currentSong = updateCurrentSong();
+});
+client.on('new-playhistory', async () => {
+    songHistory = getSongHistory();
+});
+client.on('disconnect', () => {
+    console.log(`Socket.io-client disconnected`);
+});
+
+function pressButton() {
+
+}
+
