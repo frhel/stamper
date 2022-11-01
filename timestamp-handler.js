afcc51537a10befde34dd9a9c9b23b2f0e96d182
Freyr H <freyr.helga@gmail.com>
2022-11-01 20:38:59 +0000

    - Add sessiondata.json file to store session data - Add processNewTimeStamp() to handle changes to session data - Add addSessionEntry() to write new session data - Add loadSessionData() to load sessionData from json file - Add resetSessionData() to clear the sessiondata from file - Add checkSessionExpired() to check if session is older than 12h

--- timestamp-handler.js
+++ timestamp-handler.js
@@ -1,9 +1,47 @@
 import fs from 'fs';
 import readline from 'readline';
 
-async function processNewTimeStamp() {
+async function processNewTimeStamp(currentSong, songHistory) {
     const timestamp = await getLatestTimeStamp();
+    const sessionData = await loadSessionData();
+
+    console.log(sessionData);
+
+    await addSessionEntry(sessionData, currentSong, timestamp);
+}
+
+async function addSessionEntry(sessionData, currentSong, timestamp) {
+    currentSong.timestamp = timestamp;
+    sessionData.entries.push(currentSong);
+    fs.writeFileSync('sessiondata.json', JSON.stringify(sessionData), 'utf8'); 
+}
+
+async function loadSessionData() {
+    var obj = JSON.parse(fs.readFileSync('sessiondata.json', 'utf8'));
     
+    // Check if last sessions started more than 12 hours ago and reset it if it has
+    if (await checkSessionExpired(obj.sessionTimeStarted)) {
+        await resetSessionData();
+    }
+
+    return obj;
+}
+
+async function resetSessionData() {
+    const resetObj = {
+        sessionTimeStarted: Date.now(),
+        entries: [],
+    }
+    fs.writeFileSync('sessiondata.json', JSON.stringify(resetObj), 'utf8');
+}
+
+async function checkSessionExpired(sessionTimeStarted) {
+    const sessionTimerCount = 1000 * 60 * 60 * 12;
+    const sessionTimerAge = Date.now() - sessionTimerCount;
+    console.log(sessionTimerAge);
+    if (sessionTimerAge > sessionTimeStarted) {
+        return true;
+    }
 }
 
 async function getLatestTimeStamp() {
