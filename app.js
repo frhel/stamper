1799aa311a4247315d530f3e6e78923d4deef0cf
Freyr H <freyr.helga@gmail.com>
2022-11-13 11:27:50 +0000

    Add Global Hotkeys

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import io from 'socket.io-client';
+import { GlobalKeyboardListener } from 'node-global-key-listener';
+
 import { updateCurrentSong, getSongHistory } from './song-handler.js';
-import { processNewTimeStamp } from './timestamp-handler.js';
+import { processNewTimeStamp, setEntryAsPlayed, checkIfExistsAndUpdate } from './timestamp-handler.js';
 
 // https://api.streamersonglist.com/docs/ for endpoints. 
 const streamerId = 7325;
@@ -11,9 +13,10 @@ let songHistory = {};
 async function setInitialValues() {
     currentSong = await updateCurrentSong();
     songHistory = await getSongHistory();
-    pushButton(currentSong, songHistory);
 }
 setInitialValues();
+
+const v = new GlobalKeyboardListener();
 
 const client = io(`https://api.streamersonglist.com`, {
     transports: ["websocket"]
@@ -27,16 +30,42 @@ client.on('connect', () => {
     
 });
 client.on('queue-update', async () => {
-    currentSong = updateCurrentSong();
+    currentSong = await updateCurrentSong();
+    checkIfExistsAndUpdate(currentSong);
 });
 client.on('new-playhistory', async () => {
-    songHistory = getSongHistory();
+    songHistory = await getSongHistory();
+    setEntryAsPlayed(songHistory);
+
 });
 client.on('disconnect', () => {
     console.log(`Socket.io-client disconnected`);
 });
 
-function pushButton(currentSong, songHistory) {
-    processNewTimeStamp(currentSong, songHistory);
+// Log keystrokes
+
+// v.addListener(function (e, down) {
+//     console.log(
+//         `${e.name} ${e.state == "DOWN" ? "DOWN" : "UP  "} [${e.rawKey._nameRaw}]`
+//     )
+// });
+
+const detectHotkey = (e, down) => {
+    if (e.state == "DOWN" && e.name == "T" 
+        && (down["LEFT ALT"] || down["RIGHT ALT"])
+        && (down["LEFT SHIFT"] || down["RIGHT SHIFT"])
+        && (down["LEFT CTRL"] || down["RIGHT CTRL"])) {
+        
+        setTimeout(() => {            
+            processNewTimeStamp(currentSong);
+        }, 1000);
+
+        // Cycle the listener to prevent repeat keystrokes
+        v.removeListener(detectHotkey);
+        setTimeout(() => {
+            v.addListener(detectHotkey);
+        }, 200);
+    }
 }
 
+v.addListener(detectHotkey);
