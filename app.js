22adae7a83f4fb7b61f614140f4c68c95ff61147
Freyr H <freyr.helga@gmail.com>
2022-11-13 22:44:04 +0000

    - Add chokidar module to watch the OBS vid folder - Responds to new .mkv files being created in OBS vid folder - Remove old way of reading timestamps from file - Calculate timestamps from current time  - .mkv file creation time - Remove session check - Reset session when new OBS recording is detected - Reset session on startup - Replace old timestamp call with new timestamp call - Add new sessiondata parameter "lastTimestamp" - Add function to revert timestamp to "lastTimestamp" - Update existing functions to take parameter from new revert function - Add global hotkey recognition for ctrl+shift+alt+R for new revert func - Add sessiondata sort(by timestamp) to youtube export function

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 import io from 'socket.io-client';
 import { GlobalKeyboardListener } from 'node-global-key-listener';
+import chokidar, { watch } from 'chokidar';
 
 import { updateCurrentSong, getSongHistory } from './song-handler.js';
-import { processNewTimeStamp, setEntryAsPlayed, checkIfExistsAndUpdate } from './timestamp-handler.js';
+import {
+    processNewTimeStamp,
+    setEntryAsPlayed,
+    checkIfExistsAndUpdate,
+    resetSessionData,
+    revertTimestamp
+    } from './timestamp-handler.js';
 
 // https://api.streamersonglist.com/docs/ for endpoints. 
 const streamerId = 7325;
 
 let currentSong = {};
 let songHistory = {};
+let startTime = 0;
 
 async function setInitialValues() {
+    resetSessionData();
     currentSong = await updateCurrentSong();
     songHistory = await getSongHistory();
 }
-setInitialValues();
 
 const v = new GlobalKeyboardListener();
 
@@ -31,7 +39,7 @@ client.on('connect', () => {
 });
 client.on('queue-update', async () => {
     currentSong = await updateCurrentSong();
-    checkIfExistsAndUpdate(currentSong);
+    checkIfExistsAndUpdate(currentSong, startTime);
 });
 client.on('new-playhistory', async () => {
     songHistory = await getSongHistory();
@@ -40,7 +48,7 @@ client.on('new-playhistory', async () => {
 });
 client.on('disconnect', () => {
     console.log(`Socket.io-client disconnected`);
-});
+}); 
 
 // Log keystrokes
 
@@ -57,7 +65,23 @@ const detectHotkey = (e, down) => {
         && (down["LEFT CTRL"] || down["RIGHT CTRL"])) {
         
         setTimeout(() => {            
-            processNewTimeStamp(currentSong);
+            processNewTimeStamp(currentSong, startTime);
+        }, 1000);
+
+        // Cycle the listener to prevent repeat keystrokes
+        v.removeListener(detectHotkey);
+        setTimeout(() => {
+            v.addListener(detectHotkey);
+        }, 200);
+    }
+
+    if (e.state == "DOWN" && e.name == "R" 
+        && (down["LEFT ALT"] || down["RIGHT ALT"])
+        && (down["LEFT SHIFT"] || down["RIGHT SHIFT"])
+        && (down["LEFT CTRL"] || down["RIGHT CTRL"])) {
+        
+        setTimeout(() => {          
+            revertTimestamp(currentSong);
         }, 1000);
 
         // Cycle the listener to prevent repeat keystrokes
@@ -69,3 +93,22 @@ const detectHotkey = (e, down) => {
 }
 
 v.addListener(detectHotkey);
+
+
+// File Watching stuff
+const watcher = chokidar.watch('*.mkv', {
+    ignored: ".mp4",
+    persistent: true,
+    followSymlinks: false,
+    cwd: "E:\\VODs\\Temp\\",
+    alwaysStat: true,
+    useFsEvents: true
+});
+
+watcher.on('add', (path, stats) => {  
+    if (stats) {
+        startTime = +stats.birthtime;
+        setInitialValues();
+        console.log(`New session initialized at ${new Date(Date.now() - startTime).toISOString()}`)
+    }
+});
